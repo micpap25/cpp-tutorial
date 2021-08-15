36201cb77589d14d7461b777cdf3cb4e49cb5489
micpap25 <micpap25@gmail.com>
2021-08-14 22:20:07 -0400

    Update CommandLineArgs.cpp

--- Chapters 10 - 13/CommandLineArgs.cpp
+++ Chapters 10 - 13/CommandLineArgs.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream> //stringstreams
 
 // argc is the number of CLAs, argv is the list of them as C-style strings.
 int main(int argc, char* argv[]){
@@ -9,5 +11,25 @@ int main(int argc, char* argv[]){
         std::cout << argv[i] << "\n";
     }
 
-    return 0;
+    if (argc <= 1)
+	{
+		// On some operating systems, argv[0] can end up as an empty string instead of the program's name.
+		// We'll conditionalize our response on whether argv[0] is empty or not.
+		if (argv[0])
+			std::cout << "Usage: " << argv[0] << " <number>" << '\n';
+		else
+			std::cout << "Usage: <program name> <number>" << '\n';
+            
+		return 1;
+	}
+ 
+	std::stringstream convert{argv[1]}; // set up a stringstream variable named convert, initialized with the input from argv[1]
+ 
+	int myint{};
+	if (!(convert >> myint)) // do the conversion
+		myint = 0; // if conversion fails, set myint to a default value
+ 
+	std::cout << "Got integer: " << myint << '\n';
+ 
+	return 0;
 }
